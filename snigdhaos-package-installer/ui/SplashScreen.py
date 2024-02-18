a41e036567231b1d066261690c6b34b81f3f21a9
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-02-18 23:58:02 +0530

    update

--- snigdhaos-package-installer/ui/SplashScreen.py
+++ snigdhaos-package-installer/ui/SplashScreen.py
@@ -2,3 +2,10 @@ import gi
 from Functions import os
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk,Gdk
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+
+class SplashScreen(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, Gtk.WindowType.POPUP, title="")
+        self.set_decorated(False)
