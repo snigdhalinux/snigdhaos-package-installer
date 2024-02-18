093910caea5b9af74280f5b457671e9f07e6d84a
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-02-19 00:07:16 +0530

    i am lazy

--- snigdhaos-package-installer/ui/SplashScreen.py
+++ snigdhaos-package-installer/ui/SplashScreen.py
@@ -1,7 +1,7 @@
 import gi
 from Functions import os
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk,Gdk
+from gi.repository import Gtk,Gdk,GdkPixbuf
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 
@@ -9,3 +9,16 @@ class SplashScreen(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, Gtk.WindowType.POPUP, title="")
         self.set_decorated(False)
+        self.set_resizable(False)
+        self.set_size_request(1280, 720)
+        self.set_position(Gtk.WindowPosition.CENTER)
+        main_vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing = 1)
+        self.add(main_vbox)
+        self.image = Gtk.Image()
+        spimage = GdkPixbuf.Pixbuf().new_from_file_at_size(
+            base_dir + "/image/snigdhaos-splash.png"
+        )
+        self.image.set_from_pixbuf(spimage)
+        main_vbox.pack_start(self.image, True, True, 0)
+        self.show_all()
+
